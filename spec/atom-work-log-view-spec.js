a1bc7f6bd4892cf3ab9770b36ef671e482d6d79f
Chris Meyer <meyer.cm@gmail.com>
2017-03-07 20:33:11 -0500

    initial commit

---
+++ spec/atom-work-log-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWorkLogView from '../lib/atom-work-log-view';
+
+describe('AtomWorkLogView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
